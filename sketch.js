259ea735e70de676e0331d3086e72871045263f6
Ulrics <66984492+Ulrics@users.noreply.github.com>
2025-05-18 00:14:53 -0700

    built change grid functionality

--- sketch.js
+++ sketch.js
@@ -1,7 +1,5 @@
 const container = document.querySelector(".sketchContainer");
 
-
-
 function buildGrid(size){
     for(let r = 0; r < size; r++){
         const row = document.createElement("div");
@@ -16,8 +14,6 @@ function buildGrid(size){
 }
 
 buildGrid(16);
-
-
 let boxes = document.querySelectorAll(".defaultStateBox");
 
 /*
@@ -36,3 +32,25 @@ boxes.forEach((box) => {
         box.classList.add("coloredStateBox");
     });
 });
+
+const changeGrid = document.querySelector(".changeGridButton");
+changeGrid.addEventListener("click", () =>{
+    let newGrid = prompt("Enter new grid size (max 100)", 16);
+    if (newGrid < 101){
+        const rowList = document.querySelectorAll(".row");
+        rowList.forEach((row) => {
+            row.remove();
+        })
+        buildGrid(newGrid);
+        let boxes = document.querySelectorAll(".defaultStateBox");
+        boxes.forEach((box) => {
+            box.addEventListener("mouseover", () => {
+                box.classList.remove("defaultStateBox");
+                box.classList.add("coloredStateBox");
+            });
+        });
+    }
+    else{
+        alert("Error: greater than 100 or not a single integer.");
+    }
+})
